625d8584b7455dca3c8b093d1aae54d51d2d30f2
ce4 <kimtestce4@gmail.com>
2024-09-08 22:51:10 +0900

    asd

--- UPT dimentions/upt.js
+++ UPT dimentions/upt.js
@@ -1,13 +1,12 @@
-UPT = 1;
-
-fc = 1;
-fc2 = 0;
-
+let loop; // loop 변수를 전역으로 선언
+let UPT = 1;
+let fc = 1;
+let fc2 = 0;
 
 function UPT1() {
     const target = document.querySelector("h2#Title2");
-    target.innerHTML = "UPT " + UPT + "개를 보유중입니다.";
-  }
+    target.innerHTML = "UPT " + UPT.toFixed(1) + "개를 보유중입니다.";
+}
 
 function UPT2() {
     const target = document.querySelector("h2#upt1cw2");
@@ -15,41 +14,42 @@ function UPT2() {
 }
 
 function UPT3() {
-  const target = document.querySelector("h2#upt1cw3");
-  target.innerHTML = "현재 소유량 : " + fc2 + "개";
+    const target = document.querySelector("h2#upt1cw3");
+    target.innerHTML = "현재 소유량 : " + fc2 + "개";
 }
 
 function UPT4() {
-  if (UPT >= fc){
-    const a = document.querySelector("img#upt10");
-    a.setAttribute("src", "upt.webp");
-
-  }
-  else if (UPT < fc){
-    const a = document.querySelector("img#upt10");
-    a.setAttribute("src", "glerm.webp");
-  }
+    if (UPT >= fc) {
+        const a = document.querySelector("img#upt10");
+        a.setAttribute("src", "upt.webp");
+    } else if (UPT < fc) {
+        const a = document.querySelector("img#upt10");
+        a.setAttribute("src", "glerm.webp");
+    }
 }
 
 function upt1cw() {
-  if (UPT < fc){
-    alert("UPT가 부족합니다!");
-  }
-  else if (UPT >= fc) {
-    UPT -= fc;
-    loop = setInterval(() => {upt1cwhp();}, 1000);
-    fc += 10;
-    fc2 += 1;
+    if (UPT < fc) {
+        alert("UPT가 부족합니다!");
+    } else if (UPT >= fc) {
+        UPT -= fc;
 
-  }
-  
+        // 이미 실행 중인 loop가 있으면 중단
+        if (loop) {
+            clearInterval(loop);
+        }
 
-    
+        // 새로운 loop 시작
+        loop = setInterval(() => { upt1cwhp(); }, 100);
+
+        fc += 10;
+        fc2 += 1;
+    }
 }
-
 
 function upt1cwhp() {
-    UPT += (1*fc2)
+    UPT += (0.1 * fc2);
 }
 
-loop1 = setInterval(() => {UPT1(); UPT2(); UPT3(); UPT4();}, 1);
+// UPT 상태 업데이트를 매 1ms마다 실행
+loop1 = setInterval(() => { UPT1(); UPT2(); UPT3(); UPT4(); }, 1);
